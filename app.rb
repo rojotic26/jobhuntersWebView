76d4a063ba1aac9978a4bc7be5c45289910e01b9
rojotic26 <rojotic26@gmail.com>
2014-11-01 23:20:51 +0800

    Adding the new function get_offers

--- app.rb
+++ app.rb
@@ -3,4 +3,19 @@ require 'jobhunters'
 require 'json'
 
 class TecolocoJobOffers < Sinatra:Base
+helpers do
+def get_offers(category)
+jobs = {
+'id' => category
+'jobs_offers' => []}
+
+cat = params[:category]
+offers = JobSearch::Tecoloco.getjobs(cat)
+
+offers.each do |title, date, city|
+jobs['jobs_offers'].push('id' => title, 'date' => date, 'city' => city)
+end
+jobs
+end
+
 end
