3d245c0943ebf290518e901c165330d378637b80
rojotic26 <rojotic26@gmail.com>
2014-11-07 16:44:52 +0800

    Adding the test of the GET route

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,3 +1,13 @@
 require_relative 'spec_helper'
 require_relative 'support/jobs_helpers.rb'
 require_relative 'json'
+
+describe 'Job Offers in CA' do
+include JobsHelpers
+
+describe 'Getting the route of the service'
+    it 'Should return ok' do
+    get '/'
+    last_response.must_be :ok?
+    end
+end
