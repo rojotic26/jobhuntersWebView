1871cb840dce0484fd5fab5648299469ad01e3f6
rojotic26 <rojotic26@gmail.com>
2014-11-07 17:06:57 +0800

    Adding the sad paths tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,7 +7,16 @@ include JobsHelpers
 
 describe 'Getting the route of the service'
     it 'Should return ok' do
-    get '/'
-    last_response.must_be :ok?
+        get '/'
+        last_response.must_be :ok?
     end
+end
+
+describe 'The sad path'
+    it 'Should return 404 for unknown categories of jobs offers' do
+        get '/api/v1/job_openings/#{random_str(35)}.json'
+        last_response.must_be :not_found?
+    end
+end
+
 end
