703b4057ade5000b0d4495e8d7ce5caa798e7c03
Edwin Mejia <eddwin@live.com>
2014-11-09 18:38:51 +0800

    fixed sad path

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -14,15 +14,16 @@ end
 
 describe 'The sad path: Get' do
     it 'Should return 404 for unknown categories of jobs offers' do
-        get '/api/v1/job_openings/#{random_str(35)}.json'
+        get "/api/v1/job_openings/#{random_string(35)}.json"
         last_response.must_be :not_found?
     end
 end
 
 describe 'The sad path: Post' do
-    it 'Should return 400 for bad request' do
-        header = {'CONTENT_TYPE' => 'application/json'}
-        body = random_string(145)
+    it 'Should return 400 for JSON Formattin' do
+        header = { 'CONTENT_TYPE' => 'application/json' }
+        body = random_string(50)
+
         post '/api/v1/all', body, header
         last_response.must_be :bad_request?
     end
