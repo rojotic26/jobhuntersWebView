4cd4c45352fe1f1557ab527321497c87b9614910
rojotic26 <rojotic26@gmail.com>
2014-11-16 02:43:31 +0800

    Modifying the entity joboffer

--- db/migrate/20141115183120_create_joboffers.rb
+++ db/migrate/20141115183120_create_joboffers.rb
@@ -1,6 +1,11 @@
 class CreateJoboffers < ActiveRecord::Migration
   def self.up
   create_table :joboffers do |j|
+  j.string :title
+  j.date :date
+  j.string :city
+  j.text :details
+  j.timestamps
   end
   end
   def self.down
