ba6791b34e0b726a292e72b0e371efce2ccf47d4
Edwin Mejia <eddwin@live.com>
2014-11-24 00:46:11 +0800

    Fixed error

--- app.rb
+++ app.rb
@@ -13,93 +13,93 @@ class TecolocoJobOffers < Sinatra::Base
   configure :production, :development do
     enable :logging
   end
-helpers do
-  def offerobject
+  helpers do
+    def offerobject
       category = params[:category]
       return nil unless category
       catego = { 'id' => category , 'offers' => [], }
 
       begin
         JobSearch::Tecoloco.getjobs(category).each do |title, date, cities, details|
-        catego['offers'].push('title'=>title,'date'=>date,'city'=>cities, 'details'=>details)
-      end
-      catego
+          catego['offers'].push('title'=>title,'date'=>date,'city'=>cities, 'details'=>details)
+        end
+        catego
       rescue
         nil
       end
-  end
-  def get_jobs(category)
-    jobs_after = {
-      'type of job' => category,
-      'kind' => 'openings',
-      'jobs' => []
-    }
-
-    category = params[:category]
-    JobSearch::Tecoloco.getjobs(category).each do |title, date, cities, details|
-      jobs_after['jobs'].push('id' => title, 'date' => date, 'city' => cities, 'details'=>details)
     end
-    jobs_after
-  end
+    def get_jobs(category)
+      jobs_after = {
+        'type of job' => category,
+        'kind' => 'openings',
+        'jobs' => []
+      }
 
-
-  #Defining the function get_jobs_cat_city
-  def get_jobs_cat_city(category,city)
-    jobs_after_city = {
-      'type of job' => category,
-      'kind' => 'openings',
-      'city' => city,
-      'jobs' => []
-    }
-    flag=false
-    category = params[:category]
-    city = params[:city]
-    JobSearch::Tecoloco.getjobs(category).each do |title, date, cities|
-      if cities.to_s == city.to_s
-        flag=true
-        jobs_after_city['jobs'].push('id' => title, 'date' => date)
+      category = params[:category]
+      JobSearch::Tecoloco.getjobs(category).each do |title, date, cities, details|
+        jobs_after['jobs'].push('id' => title, 'date' => date, 'city' => cities, 'details'=>details)
       end
+      jobs_after
     end
-    if flag==false then
-    halt 404
-    else
-    jobs_after_city
+
+
+    #Defining the function get_jobs_cat_city
+    def get_jobs_cat_city(category,city)
+      jobs_after_city = {
+        'type of job' => category,
+        'kind' => 'openings',
+        'city' => city,
+        'jobs' => []
+      }
+      flag=false
+      category = params[:category]
+      city = params[:city]
+      JobSearch::Tecoloco.getjobs(category).each do |title, date, cities|
+        if cities.to_s == city.to_s
+          flag=true
+          jobs_after_city['jobs'].push('id' => title, 'date' => date)
+        end
+      end
+      if flag==false then
+        halt 404
+      else
+        jobs_after_city
+      end
+
+    end
+
+
+    #Defining the function get_jobs_city
+    def check_cat(category)
+      ##Checks if Category exists within Tecoloco
+
+      case category
+      when  "marketing"
+        @output = "marketing-ventas"
+      when "banca"
+        @output = "banco-servicios-financieros"
+      else
+        @output = "none"
+      end
+      @output
+    end
+
+    def list_joboffers(categories)
+      @list_all = {}
+      categories.each do |category|
+        @list_all[category] = JobSearch::Tecoloco.getjobs(category)
+      end
+      @list_all
+    end
+
+    def current_page?(path = ' ')
+      path_info = request.path_info
+      path_info += ' ' if path_info == '/'
+      request_path = path_info.split '/'
+      request_path[1] == path
     end
 
   end
-
-
-  #Defining the function get_jobs_city
-  def check_cat(category)
-   ##Checks if Category exists within Tecoloco
-
-   case category
-   when  "marketing"
-      @output = "marketing-ventas"
-   when "banca"
-      @output = "banco-servicios-financieros"
-    else
-      @output = "none"
-    end
-    @output
-  end
-
-  def list_joboffers(categories)
-    @list_all = {}
-    categories.each do |category|
-      @list_all[category] = JobSearch::Tecoloco.getjobs(category)
-    end
-    @list_all
-  end
-
-  def current_page?(path = ' ')
-    path_info = request.path_info
-    path_info += ' ' if path_info == '/'
-    request_path = path_info.split '/'
-    request_path[1] == path
-  end
-
-end
 
 
   get '/' do
@@ -109,12 +109,12 @@ end
   get '/api/v1/job_openings/:category.json' do
     cat = params[:category]
     category_url = check_cat(cat)
-      if category_url == "none" then
-        halt 404
-      else
-        content_type :json
-        get_jobs(category_url).to_json
-      end
+    if category_url == "none" then
+      halt 404
+    else
+      content_type :json
+      get_jobs(category_url).to_json
+    end
 
   end
 
@@ -130,7 +130,7 @@ end
       req = JSON.parse(request.body.read)
       logger.info req
     rescue
-        halt 400
+      halt 400
     end
 
     cat = Category.new
@@ -141,7 +141,7 @@ end
     end
   end
 
-    get '/api/v1/offers/:id' do
+  get '/api/v1/offers/:id' do
     content_type:json
     begin
       @category = Category.find(params[:id])
@@ -150,34 +150,33 @@ end
     rescue
       halt 400
     end
-      list_joboffers(cat).to_json
-    end
+    list_joboffers(cat).to_json
+  end
 
-    get '/joboffers' do
+  get '/joboffers' do
 
     @category = params[:category]
     if @category
       redirect "/joboffers/#{@category}"
       return nil
     end
-      haml :joboffers
+    haml :joboffers
+  end
+
+  get '/joboffers/:category' do
+    @jobofferobject = offerobject
+    @category = params[:category]
+
+    if @category && @jobofferobject.nil?
+      flash[:notice] = 'Category not found' if @jobofferobject.nil?
+      redirect '/joboffers'
     end
+    haml :joboffers
+  end
 
-    get '/joboffers/:category' do
-      @jobofferobject = offerobject
-      @category = params[:category]
-
-      if @category && @jobofferobject.nil?
-        flash[:notice] = 'Category not found' if @jobofferobject.nil?
-        redirect '/joboffers'
-      end
-
-      haml :joboffers
-    end
-
-    get '/aboutus' do
-      haml :aboutus
-    end
+  get '/aboutus' do
+    haml :aboutus
+  end
 
 
 
